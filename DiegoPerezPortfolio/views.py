8c3b1306d73b1c1acf5e5319b0e26b7b16a844e9
Diego Perez <u1301598@utah.edu>
2024-12-31 16:12:21 -0700

    minor

--- DiegoPerezPortfolio/views.py
+++ DiegoPerezPortfolio/views.py
@@ -7,7 +7,8 @@ from DiegoPerezPortfolio.models import Project
 
 
 def index(request):
-    return render(request, 'index.html')
+    projects = Project.objects.all()
+    return render(request, 'index.html', {'projects': projects})
 
 def project(request, project_name):
     curr_project = get_object_or_404(Project, title=project_name)
