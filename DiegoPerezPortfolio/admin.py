0dfc4c9f1b1df9355ff99134174bef89f7cb6eeb
Diego Perez <u1301598@utah.edu>
2024-12-31 14:05:58 -0700

    Project are now in static
    - Only thumbnails should be stored in the actual

--- DiegoPerezPortfolio/admin.py
+++ DiegoPerezPortfolio/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project, ProjectImages
+from .models import Project
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title',)
-
-@admin.register(ProjectImages)
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ()
