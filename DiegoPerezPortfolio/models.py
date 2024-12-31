370f28a5785f9babdd3d0a439c704f85a0406735
Diego Perez <u1301598@utah.edu>
2024-12-31 14:12:12 -0700

    Minor Model Change

--- DiegoPerezPortfolio/models.py
+++ DiegoPerezPortfolio/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    thumbnail = models.ImageField(blank=True, null=True)
     ## need a way for a list of images maybe
     def __str__(self):
         return self.title
