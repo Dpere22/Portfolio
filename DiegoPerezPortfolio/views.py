ffe28bb47ac8c2e1568d831dbe8bb49bf826cd55
Diego Perez <u1301598@utah.edu>
2025-01-11 19:10:38 -0700

    Major CSS Upgrade and About me
    - Auto sizing now works more reliably
    - About me page added

--- DiegoPerezPortfolio/views.py
+++ DiegoPerezPortfolio/views.py
@@ -13,3 +13,6 @@ def index(request):
 def project(request, project_name):
     curr_project = get_object_or_404(Project, title=project_name)
     return render(request, 'project.html', context={'project': curr_project})
+
+def aboutme(request):
+    return render(request, 'aboutme.html')
--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('<str:project_name>/', views.project),
+    path('aboutme', views.aboutme),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
