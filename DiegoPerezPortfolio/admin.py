fd8be6d1c29167e4821bcb311762146e83487692
Diego Perez <u1301598@utah.edu>
2025-01-11 21:54:04 -0700

    Project Pages CSS should be good

--- DiegoPerezPortfolio/admin.py
+++ DiegoPerezPortfolio/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Project
+from .models import Project, ProjectImages
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title',)
+
+@admin.register(ProjectImages)
+class ProjectImagesAdmin(admin.ModelAdmin):
+    list_display = ('image',)
--- DiegoPerezPortfolio/models.py
+++ DiegoPerezPortfolio/models.py
@@ -8,3 +8,10 @@ class Project(models.Model):
     ## need a way for a list of images maybe
     def __str__(self):
         return self.title
+
+class ProjectImages(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField()
+    caption = models.TextField(blank = True)
+    def __str__(self):
+        return f"Image for {self.project.title}"
