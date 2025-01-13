df420590c9336ac1a87643ffdeb4b28b7c87d09a
Diego Perez <u1301598@utah.edu>
2025-01-12 17:00:47 -0700

    Images now delete when project's are deleted

--- DiegoPerezPortfolio/models.py
+++ DiegoPerezPortfolio/models.py
@@ -1,4 +1,9 @@
+import os
+
 from django.db import models
+
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 # Create your models here.
 class Project(models.Model):
@@ -9,9 +14,22 @@ class Project(models.Model):
     def __str__(self):
         return self.title
 
+@receiver(post_delete, sender=Project)
+def delete_image_on_project_delete(sender, instance, **kwargs):
+    if instance.thumbnail:
+        if os.path.isfile(instance.thumbnail.path):
+            os.remove(instance.thumbnail.path)
+
 class ProjectImages(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField()
     caption = models.TextField(blank = True)
     def __str__(self):
         return f"Image for {self.project.title}"
+    ##note, if in admin add pictures with path /media/filename
+
+@receiver(post_delete, sender=ProjectImages)
+def delete_images_on_project_delete(sender, instance, **kwargs):
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
